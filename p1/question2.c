bde1b9da488741ee28991ccf91860ad9dcc4899c
dbusan <daniel.busan+git@gmail.com>
2015-08-13 13:04:09 +0800

    Added question 2
    Reads 2 integers. Prints "divisible" if first number is divisible by the
    second; Prints "Not divisible" otherwise.

---
+++ p1/question2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b;
+	
+	printf("Enter 2 integers\n> "); scanf("%d %d", &a, &b);
+	
+	if (a % b == 0)
+	{
+		printf("Divisible \n");
+	}
+	else
+		printf("Not divisible \n");
+	return 0;
+}
