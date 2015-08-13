3ed1f68f438fecc734fbe3625b106101f4260013
dbusan <daniel.busan+git@gmail.com>
2015-08-13 14:21:31 +0800

    Practical 2, macros
    Practical 2. We are asked to create the .h file "download.h" while
    download.c is provided to test the macros.

---
+++ p2/download.h
@@ -0,0 +1,9 @@
+#include <time.h>
+
+#define ELAPSED_TIME(time, startTime) ((time)-(startTime))
+#define PERCENT_COMPLETE(bytes, totalBytes) ((bytes)/(totalBytes)*100)
+#define DOWNLOAD_SPEED(time, startTime, bytes) ( (bytes)/(ELAPSED_TIME(time, startTime)) )
+#define TOTAL_TIME(time, startTime, bytes, totalBytes) ((totalBytes)/(DOWNLOAD_SPEED(time, startTime, bytes)))
+#define REMAINING_TIME(time, startTime, bytes, totalBytes) ( (TOTAL_TIME(time, startTime, bytes, totalBytes)) - (ELAPSED_TIME(time, startTime)))
+
+
