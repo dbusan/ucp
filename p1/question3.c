82e23089f9f86b72ff498df9f32082982c188614
dbusan <daniel.busan+git@gmail.com>
2015-08-13 13:43:00 +0800

    Added question 3
    Computes factorial of inputted number.  If number input is negative,
    requests another input.

---
+++ p1/question3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int factorial (int);
+
+int main(void)
+{
+  int n;
+  do {
+    printf("\nEnter a number:\n> ");
+    scanf("%d", &n);
+    if (n < 0)
+    {
+      printf("Number is negative. Please try again\n", );
+    }
+  } while (n<1);
+  printf("\n%d! = %d\n", n, factorial(n));
+  return 0;
+}
+
+int factorial(int n) // function that computes the factorial of n
+{
+  int result = 1;
+  int i;
+
+  for (i = 1; i <= n; i++)
+  {
+    result *= i;  // even if n is zero, the returned value
+                  // is 1
+  }
+  return result;
+}
